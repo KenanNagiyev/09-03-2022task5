92d7b88c36c067b6e66b57fec2903947113e2f7e
KenanNagiyev <788lnmp@code.edu.az>
2022-03-08 21:41:50 +0400

    new file

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+              
+            static void Main(string[] args)
+            {
+                Console.WriteLine(HasLetter("cevre", 'a'));
+            }
+            static bool HasLetter(string name, char letter)
+            {
+                for (int i = 0; i < name.Length; i++)
+                {
+                    if (name[i] == letter)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+    }
+}
